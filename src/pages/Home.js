bab396c87666db18623d4532bb7eb2eb634d6019
SubhamGaikwad <122010448+SubhamGaikwad@users.noreply.github.com>
2023-10-08 18:20:15 +0530

    created frontend part of inventory

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+const Home = () => {
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    !user && navigate("/login", { replace: true });
+  }, []);
+  return (
+    <>
+      <div className="jumbotron">
+        <h1>Welcome {user ? user.name : null}</h1>
+        <hr className="my-4" />
+        <Link to="/mycontacts">
+          <a className="btn btn-info" href="#" role="button">
+            Add Products
+          </a>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Home;
