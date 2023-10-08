c65aecc39c5f706438a494a61399077f64815b4a
SubhamGaikwad <122010448+SubhamGaikwad@users.noreply.github.com>
2023-10-08 21:03:17 +0530

    removed some unusual code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useEffect } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 const Home = () => {
